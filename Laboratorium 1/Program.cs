e7258f748cc07ef9220433807f7a6fe350f0f01a
Szymon-Pawlik <szymon.pawlik@microsoft.wsei.edu.pl>
2023-10-04 18:52:06 +0200

    z`

--- Laboratorium 1/Program.cs
+++ Laboratorium 1/Program.cs
@@ -2,9 +2,12 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello WSEI");
+            Console.ReadKey();
+
         }
     }
 }
